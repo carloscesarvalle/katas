bb0d9859803ff0dab44b64069ea15a866840c0fa
carloscesarvalle <carloscesarvalle@gmail.com>
2020-10-05 23:29:56 -0400

    another 7kyu kata solved

--- 6_kyu/Duplicate_Encoder.js
+++ 6_kyu/Duplicate_Encoder.js
@@ -84,7 +84,7 @@ const duplicateEncode = str =>  {
   lowerCaseString = str.toLowerCase()
 //  "/./" is to refer to every element from the string
 // "g" tells to not to stop after replacing the first element that follows the condition, but all elements that follow the condition
-  return lowerCaseString.replace(/./g, element =>  lowerCaseString.indexOf(element)===  lowerCaseString.lastIndexOf(element)? '(' : ')')
+  return lowerCaseString.replace(/./g, element =>  lowerCaseString.indexOf(element) ===  lowerCaseString.lastIndexOf(element)? '(' : ')')
   }
   
 
---
+++ 7_kyu/Sum_of_Array_Singles.js
@@ -0,0 +1,21 @@
+// In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15.
+
+
+
+// Solution using map, terciary operator and reduce
+
+function repeats(arr){
+
+    return arr
+    //I'll map thru the array in order to get rid of repeated elements. A new array (unnamed) is created inside the function.
+    //The unnamed array contains the elements that passed the terciary operator condition.
+    //The result is an array with unique values and zeros.
+    .map((element, index, array) => { return array.indexOf(element) === array.lastIndexOf(element) ? element : 0 })
+    //I will sum all the unique values and the zeros from the unnamed array using reduce.
+    //In arrow functions with simple syntax we don't need return.
+    .reduce((accumulator, currentValue) => accumulator + currentValue)
+   
+};
+
